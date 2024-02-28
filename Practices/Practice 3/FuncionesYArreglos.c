75bcff352bdeabb0a7f9f031cf6e718ed6c9eef6
OsmarGHz <osmarjavier1@gmail.com>
2024-02-27 19:26:34 -0600

    Version funcional (sin funciones) de Practica3, y agregando gitignore

--- Practices/Practice 3/FuncionesYArreglos.c
+++ Practices/Practice 3/FuncionesYArreglos.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <string.h>
-#define TAMBUT 20
+#define TAMAL 20
 
 int main(){
-    int inscritos[TAMBUT], butacas[TAMBUT];
-    int realmenteInscritos,aTiempo,aTarde,i,j;
+    int inscritos[TAMAL], butacas[TAMAL];
+    int realmenteInscritos,aTiempo,tarde,i,j,k,temp,tempIt;
     short error=0,coincidencia=0;
     printf("Cuantos alumnos hay inscritos? ");
     scanf("%d",&realmenteInscritos);
@@ -32,20 +32,94 @@ int main(){
     i=0;
     while (i<aTiempo){
         printf("Ingrese la matricula del alumno %d (entre 1010 y 1200)", i+1);
-        scanf("%d",&butacas[i]);
-        if (inscritos[i]<1010 || inscritos[i]>1200){
+        scanf("%d",&temp);
+        if (temp<1010 || temp>1200){
             printf("\n\t\t * Matricula incorrecta. Favor de ingresar otra.  *\n");
         }else{
             coincidencia=0;
-            for (j = 0; j < i; j++){
-                if (inscritos[j]==inscritos[i]){
-                    printf("\n\t\t * Matricula repetida. Favor de ingresar otra.  *\n");
-                    error++;
+            for (j = 0; j < realmenteInscritos; j++){
+                if (temp==inscritos[j]){
+                    printf("\n\t\t * Alumno encontrado... *\n");
+                    for (k = 0; k < i; k++){
+                        if (butacas[k]==j){
+                            coincidencia++;
+                        }
+                    }
+                    tempIt=j;
+                    coincidencia++;
                 }
             }
-            if (error==0){
+            if (coincidencia==1){
+                printf("Registrado\n");
+                butacas[i]=tempIt;
                 i++;
+            }else if(coincidencia>1){
+                printf("\n\t\t * Esta matricula ya tiene lugar. Favor de ingresar otra.  *\n");
+            }else{
+                printf("\n\t\t * No se encontro el alumno. *\n");
             }
         }
     }
+
+    printf("Cuantos alumnos llegaron TARDE? ");
+    scanf("%d",&tarde);
+    while (i<(aTiempo+tarde)){
+        printf("Ingrese la matricula del alumno %d (entre 1010 y 1200)", i+1);
+        scanf("%d",&temp);
+        if (temp<1010 || temp>1200){
+            printf("\n\t\t * Matricula incorrecta. Favor de ingresar otra.  *\n");
+        }else{
+            coincidencia=0;
+            for (j = 0; j < realmenteInscritos; j++){
+                if (temp==inscritos[j]){
+                    printf("\n\t\t * Alumno encontrado... *\n");
+                    for (k = 0; k < i; k++){
+                        if (butacas[k]==j){
+                            coincidencia++;
+                        }
+                    }
+                    tempIt=j;
+                    coincidencia++;
+                }
+            }
+            if (coincidencia==1){
+                printf("Registrado\n");
+                butacas[i]=tempIt;
+                i++;
+            }else if(coincidencia>1){
+                printf("\n\t\t * Esta matricula ya tiene lugar. Favor de ingresar otra.  *\n");
+            }else{
+                printf("\n\t\t * No se encontro el alumno. *\n");
+            }
+        }
+    }
+
+    printf("\n********************* REPORTE DE ASISTENCIA *************************\n");
+    printf("\nAlumnos inscritos: %d", realmenteInscritos);
+    printf("\nAlumnos que llegaron a tiempo\n");
+    printf("\nNo. de butaca\tNo. de lista\tMatricula");
+    for (i = 0; i < aTiempo; i++){
+        printf("\n\t%d\t %d\t %d",i,butacas[i],inscritos[butacas[i]]);
+    }
+    printf("\nAlumnos que llegaron tarde\n");
+    printf("\nNo. de butaca\tNo. de lista\tMatricula");
+    for (; i < (aTiempo+tarde); i++){
+        printf("\n\t%d\t %d\t %d",i,butacas[i],inscritos[butacas[i]]);
+    }
+
+    printf("\nAlumnos que llegaron tarde\n");
+    printf("\nNo. de lista\tMatricula");
+    for (i = 0; i < realmenteInscritos; i++){
+        coincidencia=0;
+        for (j = 0; j < (aTiempo+tarde); j++){
+            if (i==butacas[j]){
+                coincidencia++;
+            }
+        }
+        if (coincidencia==0){
+            printf("\n\t%d\t %d",i,inscritos[i]);
+        }
+        
+    }
+    
 }
